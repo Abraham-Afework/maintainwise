a428ead902ddac8fad1b90748906c76fe6d8888c
Abraham Afework <Abrahamafework7@gmail.com>
2024-06-27 00:07:09 +0200

    template url amended

--- maintainwise/settings.py
+++ maintainwise/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,7 +56,11 @@ ROOT_URLCONF = 'maintainwise.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'digital_resume'),
+            os.path.join(BASE_DIR, 'templates', 'miscellaneous'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -67,7 +72,7 @@ TEMPLATES = [
         },
     },
 ]
-
+       
 WSGI_APPLICATION = 'maintainwise.wsgi.application'
 
 
