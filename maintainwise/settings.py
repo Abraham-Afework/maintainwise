619a829b5d604f4ea6bbd789cbfe528407984fcb
Abraham Afework <Abrahamafework7@gmail.com>
2024-06-27 00:32:58 +0200

    please work

--- maintainwise/settings.py
+++ maintainwise/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -132,5 +132,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # see https://help.pythonanywhere.com/pages/DjangoStaticFiles for more info
 MEDIA_ROOT = '/home/maintainwise/maintainwise/media'
 MEDIA_URL = '/media/'
+
 STATIC_ROOT = '/home/maintainwise/maintainwise/static'
 STATIC_URL = '/static/'
